6307a8a92a27afa3bbf25f199b4d5d3ed74c2c76
Julian Garcia <julianhus.hotmail.com>
2018-12-05 16:39:17 -0500

    Integrate google maps in activity 2.2

--- app/src/main/java/com/traffico/manhattan/MapsActivity.java
+++ app/src/main/java/com/traffico/manhattan/MapsActivity.java
@@ -1,9 +1,129 @@
 package com.traffico.manhattan;
 
 import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+
+public class MapsActivity extends AppCompatActivity implements
+        GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener,
+        OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    //
+    String llamada;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        //
+        Intent iMapsActivity = getIntent();
+        llamada = (String) iMapsActivity.getSerializableExtra("Llamada");
+    }
+
+    public void onBackPressed() {
+        Intent iMaps;
+        switch (llamada) {
+            case "MainActivity":
+                iMaps = new Intent(MapsActivity.this, MainActivity.class);
+                startActivity(iMaps);
+                finish();
+                break;
+            case "EditProfileActivity":
+                iMaps = new Intent(MapsActivity.this, EditProfileActivity.class);
+                startActivity(iMaps);
+                finish();
+                break;
+            case "StoreActivity":
+                iMaps = new Intent(MapsActivity.this, StoreActivity.class);
+                startActivity(iMaps);
+                finish();
+                break;
+            default:
+                Toast.makeText(getApplicationContext(), R.string.fail, Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        try {
+            mMap = googleMap;
+            LatLng ubicacion = null;
+            switch (llamada) {
+                case "EditProfileActivity":
+                    break;
+                default:
+                    LatLng colombia = new LatLng(4.6420884, -72.834157);;
+                    ubicacion = colombia;
+                    break;
+            }
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(ubicacion));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacion, 5));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(ubicacion));
+            mMap.setOnMyLocationButtonClickListener(this);
+            mMap.setOnMyLocationClickListener(this);
+            enableMyLocation();
+            //
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), R.string.fail, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        return false;
+    }
+
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+
+    }
+
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(this, LOCATION_PERMISSION_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setZoomControlsEnabled(true);
+            mMap.getUiSettings().setCompassEnabled(true);
+            //
+        }
+    }
+}
+/*
+package com.traffico.manhattan;
+
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Camera;
 import android.location.Location;
 import android.support.annotation.NonNull;
@@ -18,6 +138,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.traffico.manhattan.clases.MyOpenHelper;
+import com.traffico.manhattan.entidades.Usuario;
 
 
 public class MapsActivity extends AppCompatActivity implements
@@ -83,22 +205,37 @@ public class MapsActivity extends AppCompatActivity implements
     public void onMapReady(GoogleMap googleMap) {
         try {
             mMap = googleMap;
-            LatLng colombia;
-
+            LatLng colombia = null;
+            MarkerOptions markerOptions = new MarkerOptions();
             if (lat == null) {
                 colombia = new LatLng(4.6420884, -72.834157);
             } else {
                 double iLat = Double.parseDouble(lat);
                 double iLon = Double.parseDouble(lon);
-                colombia = new LatLng(iLat, iLon);
-                MarkerOptions markerOptions = new MarkerOptions();
-                markerOptions.position(colombia);
-                markerOptions.title("Residencia");
-                mMap.animateCamera(CameraUpdateFactory.newLatLng(colombia));
-                mMap.addMarker(markerOptions);
+                switch (llamada) {
+                    case "EditProfileActivity":
+                        colombia = new LatLng(iLat, iLon);
+                        markerOptions.position(colombia);
+                        markerOptions.title("Tu Residencia");
+                        break;
+                    case "StoreActivity":
+                        Usuario usuario = cargaUsuario();
+                        int flagPos = usuario.getCoordenadas().indexOf(":");
+                        lat = usuario.getCoordenadas().substring(0, flagPos);
+                        lon = usuario.getCoordenadas().substring(flagPos + 1, usuario.getCoordenadas().length());
+                        iLat = Double.parseDouble(lat);
+                        iLon = Double.parseDouble(lon);
+                        colombia = new LatLng(iLat, iLon);
+                        markerOptions = new MarkerOptions();
+                        markerOptions.position(colombia);
+                        markerOptions.title("Tu Residencia");
+                        break;
+                }
             }
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(colombia));
+            mMap.addMarker(markerOptions);
             mMap.moveCamera(CameraUpdateFactory.newLatLng(colombia));
-            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(colombia, 5));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(colombia, 10));
             mMap.animateCamera(CameraUpdateFactory.newLatLng(colombia));
             mMap.setOnMyLocationButtonClickListener(this);
             mMap.setOnMyLocationClickListener(this);
@@ -142,10 +279,10 @@ public class MapsActivity extends AppCompatActivity implements
                     // This will be displayed on taping the marker
                     switch (llamada) {
                         case "EditProfileActivity":
-                            markerOptions.title("Residencia");
+                            markerOptions.title("Tu Residencia");
                             break;
                         case "MainActivity":
-                            markerOptions.title("Residencia");
+                            markerOptions.title("Tu Residencia");
                             break;
                         case "StoreActivity":
                             markerOptions.title("Tienda");
@@ -209,4 +346,16 @@ public class MapsActivity extends AppCompatActivity implements
             //
         }
     }
+
+    private Usuario cargaUsuario() {
+        Usuario usuario = new Usuario();
+        MyOpenHelper dbHelper = new MyOpenHelper(this);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        if (db != null) {
+            usuario = dbHelper.getUsuario(db);
+        }
+        return usuario;
+    }
 }
+
+*/
